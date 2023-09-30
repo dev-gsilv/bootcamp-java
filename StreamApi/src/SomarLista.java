6d9e56507b1876ff3567c02360efe5307175d243
dev-gsilv <dev.gsilv@gmail.com>
2023-09-29 22:11:35 -0300

    Exercício 8 de Stream API proposto pela Instrutora Camila.

---
+++ StreamApi/src/SomarLista.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class SomarLista {
+    // Desafio 8 - Somar os dígitos de todos os números da lista
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        int somaLista = numeros.stream().reduce(0, Integer::sum);
+
+        System.out.println("A soma dos números da lista é: "+somaLista);
+    }
+}
