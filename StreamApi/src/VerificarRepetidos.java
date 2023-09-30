adf69dac7dc925f918630cc80b5a845c8c85d5e8
dev-gsilv <dev.gsilv@gmail.com>
2023-09-30 01:40:10 -0300

    Refatorado: Exercício 9 de Stream API proposto pela Instrutora Camila.

--- StreamApi/src/VerificarRepetidos.java
+++ StreamApi/src/VerificarRepetidos.java
@@ -2,24 +2,27 @@ import java.util.*;
 public class VerificarRepetidos {
     // Desafio 9 - Verificar se todos os números da lista são distintos (não se repetem)
     public static void main(String[] args) {
-        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        List<Integer> numeros = Arrays.asList(11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
         System.out.println("Os números únicos da lista são: "+checarNumerosDistintos(numeros));
     }
 
     public static List<Integer> checarNumerosDistintos(List<Integer> lista){
-        int[] contador = new int[lista.size()*10];
-        Map<Integer, Integer> contagem = new HashMap<>();
-        List<Integer> unicos = new ArrayList<>();
+        Object[] listaOrdenada = lista.stream().sorted().toArray();
+        int valorMax = (Integer) listaOrdenada[listaOrdenada.length-1];
+        int[] contador = new int[valorMax+1];
+        // LinkedHashMap<>() mantem os elemente na ordem de inserção. HashMap<>() ordena os elementos na ordem natural.
+        Map<Integer, Integer> contagem = new LinkedHashMap<>();
+        List<Integer> listaUnicos = new ArrayList<>();
 
         lista.forEach(n -> contagem.put(n, contador[n] += 1));
 
-        for(Map.Entry<Integer, Integer> c : contagem.entrySet()){
-            if (c.getValue() == 1){
-                unicos.add(c.getKey());
+        contagem.forEach((key, value) -> {
+            if (value == 1) {
+                listaUnicos.add(key);
             }
-        }
-        return unicos;
+        });
+        return listaUnicos;
     }
 }
 
